5e955db1784f49f9a29cc81ca9a3d5337bb2e87c
Gabriel Falcão <gabriel@nacaolivre.org>
2020-01-24 01:46:08 +0100

    move config to separate file and fix tests

---
+++ flaskhello/config.py
@@ -0,0 +1,29 @@
+import os
+import redis
+
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+if REDIS_HOST:
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.Redis(
+        host=REDIS_HOST or "localhost",
+        port=int(os.getenv("REDIS_PORT") or 6379),
+        db=0,
+    )
+else:
+    SESSION_TYPE = "null"
+
+
+SECRET_KEY = b"c]WNEy-&?;NN%UzOc"
+SESSION_TYPE = "redis"
+AUTH0_DOMAIN = "dev-newstore.auth0.com"
+AUTH0_CALLBACK_URI = (
+    "https://newstoresauth0ldap.ngrok.io/callback/auth0")
+AUTH0_CLIENT_ID = "1J2tdk1J6hjloR3arVH4cZHfOor71uab"
+AUTH0_CLIENT_SECRET = (
+    "O3-tFXQSf0L9Z-dNq4XQ0gFEboJJQQYwnd9eKVKKbX3FhGIAfWgUOkBcStQKSyp2"
+)
+
+# https://manage.auth0.com/dashboard/us/dev-newstore/apis/5e2a25ea4d0204077e6a2fd5/settings
+AUTH0_API_ID = '5e2a25ea4d0204077e6a2fd5'
+AUTH0_API_IDENTIFIER = "https://NA-40801/poc-ldap"
--- flaskhello/core.py
+++ flaskhello/core.py
@@ -35,24 +35,7 @@ params = {
 }
 
 application = Flask(__name__, **params)
-application.config["SESSION_REDIS"] = redis.Redis(
-    host=os.getenv("REDIS_HOST") or "localhost",
-    port=int(os.getenv("REDIS_PORT") or 6379),
-    db=0,
-)
-application.config["SECRET_KEY"] = b"c]WNEy-&?;NN%UzOc"
-application.config["SESSION_TYPE"] = "redis"
-application.config["AUTH0_DOMAIN"] = "dev-newstore.auth0.com"
-application.config["AUTH0_CALLBACK_URI"] = (
-    "https://newstoresauth0ldap.ngrok.io/callback/auth0")
-application.config["AUTH0_CLIENT_ID"] = "N6l4Wi2JmIh5gXiGj2sibsZiJRJu0jj1"
-application.config["AUTH0_CLIENT_SECRET"] = (
-    "QaAD-WTxpqa3xUChuqyYiEL1d0bnDuusJvtij_cxgiZ9gBtww5QMkKoeabHpuwsL"
-)
-
-# https://manage.auth0.com/dashboard/us/dev-newstore/apis/5e2a25ea4d0204077e6a2fd5/settings
-application.config["AUTH0_API_ID"] = '5e2a25ea4d0204077e6a2fd5'
-application.config["AUTH0_API_IDENTIFIER"] = "https://NA-40801/poc-ldap"
+application.config.from_object('flaskhello.config')
 
 cors = CORS(application, resources="/*")
 
