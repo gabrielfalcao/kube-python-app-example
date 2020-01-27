40aa691b14ea8d9704226d77450960e95c407f66
Gabriel Falcão <gabriel@nacaolivre.org>
2020-01-27 06:48:44 +0100

    scope

--- flaskhello/models.py
+++ flaskhello/models.py
@@ -103,6 +103,10 @@ class UserToken(Model):
         return User.find_one_by(id=self.user_id)
 
     @property
+    def scope(self):
+        return self.get('scope', '').split()
+
+    @property
     def extra_data(self):
         value = self.get("extra_data")
         try:
@@ -114,5 +118,7 @@ class UserToken(Model):
     def to_dict(self):
         data = self.serialize()
         data.pop("extra_data", None)
+        data.pop("scope", None)
         data.update(self.extra_data)
+        data['scope'] = self.scope
         return data
